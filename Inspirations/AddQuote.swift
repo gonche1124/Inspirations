782f8cfc54ea0faafbb0c3d6f19f8b81e8da9953
Andres Gonzalez Casabianca <gonche1124@gmail.com>
2017-11-16 23:42:05 -0600

    - Added Color palette - Added combo box in toolbar

--- Inspirations/AddQuote.swift
+++ Inspirations/AddQuote.swift
@@ -17,16 +17,32 @@ class AddQuote: NSViewController, NSTextFieldDelegate {
         // Do view setup here.
         //doneButton.isEnabled=false
         quoteField.delegate=self
+        
+        //Add target methods.
+        tagsToken.delegate=self
+        print(tagController.arrangedObjects)
+        
+        
+        
+        //test
+//        let request = NSFetchRequest<Author>(entityName: "Author")
+//        request.predicate = NSPredicate(format: "hasQuotes.@count == 0 ")
+//        let toDelete = try! moc.fetch(request)
+//        print (toDelete)
+//        moc.delete(toDelete.first!)
+//        try! moc.save()
+        
     }
     
     //Outlets
     @IBOutlet weak var quoteField: NSTextField!
-    //@IBOutlet weak var themesField: NSTextField!
     @IBOutlet weak var doneButton: NSButton!
     @IBOutlet weak var favoriteQuote: NSButton!
     @IBOutlet weak var authorField: NSComboBox!
     @IBOutlet weak var themeField: NSComboBox!
+    @IBOutlet weak var tagsToken: NSTokenField!
     
+    @IBOutlet var tagController: NSArrayController!
     @objc dynamic lazy var moc = (NSApplication.shared.delegate as! AppDelegate).managedObjectContext
     @objc dynamic lazy var ImEx = importExport()
     
@@ -36,10 +52,10 @@ class AddQuote: NSViewController, NSTextFieldDelegate {
         
         //Get data
         let quoteT = self.quoteField.stringValue
-        //let authorT = self.authorField.stringValue
-        let themeT = self.themeField.objectValue as? String
+        let themeT = self.themeField.stringValue //self.themeField.objectValue as? String
         let isFav = self.favoriteQuote.state
-        let authorT = self.authorField.objectValue as? String
+        let authorT = self.authorField.stringValue//self.authorField.objectValue as? String
+        let tags = self.tagsToken.objectValue as? NSArray
         
         //Get context
         let appDelegate = NSApplication.shared.delegate as! AppDelegate
@@ -47,16 +63,19 @@ class AddQuote: NSViewController, NSTextFieldDelegate {
        
 
         //Create NSManagedObject
-        //let authorToAdd = Author(context: managedContext)
         let authorToAdd = ImEx.findOrCreateEntity(key: "name", value: authorT, entity: "Author", moc: managedContext) as! Author
         authorToAdd.name = authorT
-        //let authorToAdd = self.findOrCreateObject(authorName: authorT) as! Author
 
         //let quoteToAdd2 = ImEx.findOrCreateEntity(key: "quote", value: <#T##Any#>, entity: <#T##String#>, moc: <#T##NSManagedObjectContext#>)
         let quoteToAdd = Quote(context: managedContext)
         quoteToAdd.quote=quoteT
         quoteToAdd.isFavorite=Bool(truncating: isFav as NSNumber)
         quoteToAdd.fromAuthor = authorToAdd
+        for item in tags! {
+            let currTag = Tags(context: moc)
+            currTag.tag = item as? String
+            quoteToAdd.addToTags(currTag)
+        }
         
         //Create Topic
         let themeToAdd=ImEx.findOrCreateEntity(key: "topic", value: themeT, entity: "Theme", moc: managedContext) as! Theme
@@ -74,36 +93,16 @@ class AddQuote: NSViewController, NSTextFieldDelegate {
         }
         
     }
+}
+
+extension AddQuote: NSTokenFieldDelegate{
     
-    //Finds or create the object that the user input
-//    func findOrCreateObject(authorName: String)->NSManagedObject {
-//
-//        //Find object
-//        let moc = (NSApplication.shared.delegate as! AppDelegate).managedObjectContext
-//        let authorFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Author")
-//        let authorPredicate = NSPredicate(format: "name == %@", authorName)
-//        authorFetch.predicate = authorPredicate
-//
-//        //Execute fetch
-//        do {
-//            let existingAuthor = try moc.fetch(authorFetch) as! [Author]
-//
-//            if existingAuthor.count > 0 {
-//                print("Record already exists")
-//                return existingAuthor.first!
-//            }
-//            else {
-//                print("Had to create a new record")
-//                let returnedAuthor = Author(context: moc)
-//                returnedAuthor.name = authorName
-//                return returnedAuthor
-//            }
-//
-//        }
-//        catch {
-//            fatalError(error as! String)
-//        }
-//    }
+    func tokenField(_ tokenField: NSTokenField, completionsForSubstring substring: String, indexOfToken tokenIndex: Int, indexOfSelectedItem selectedIndex: UnsafeMutablePointer<Int>?) -> [Any]? {
+        return ["Happy"]
+        
+        //return tagController.arrangedObjects as! NSArray as! [Any]
+    }
+    
 }
 
 
